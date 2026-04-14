984a06a33d7cb521176bd744546411fbd1895695
mellohn <kkjjww1503@naver.com>
2026-04-14 17:54:15 +0900

    v3

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
-window._config = {
-    cognito: {
-        userPoolId: 'ap-northeast-2_j3012aIlx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'jpe2j36hk69mmt83oi8ci7oan', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-2' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: 'https://ckmukpbqok.execute-api.ap-northeast-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+    window._config = {
+        cognito: {
+            userPoolId: 'ap-northeast-2_j3012aIlx', // e.g. us-east-2_uXboG5pAb
+            userPoolClientId: 'jpe2j36hk69mmt83oi8ci7oan', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+            region: 'ap-northeast-2' // e.g. us-east-2
+        },
+        api: {
+            invokeUrl: 'https://ckmukpbqok.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        }
+    };
