d09f374b1d1e716b103f13cc45b4e8d0b60c676f
mellohn <kkjjww1503@naver.com>
2026-04-14 15:17:15 +0900

    v3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_j3012aIlx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '20o0dr16cd4e0871ormvedbfsg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'jpe2j36hk69mmt83oi8ci7oan', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
