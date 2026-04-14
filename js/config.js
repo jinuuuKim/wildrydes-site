16b453438c792712e423b2116a404c6f32c31c30
mellohn <kkjjww1503@naver.com>
2026-04-14 18:12:16 +0900

    v5

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
             region: 'ap-northeast-2' // e.g. us-east-2
         },
         api: {
-            invokeUrl: 'https://ckmukpbqok.execute-api.ap-northeast-2.amazonaws.com/prod1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+            invokeUrl: 'https://ckmukpbqok.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
         }
     };
